d7a9985d0435e86739cbc9154d416bf320e476cf
Alexander Morozov <reverse43@gmail.com>
2022-05-20 14:03:04 +0300

    Changed envs and alembic migrations to async versions, psycopg2 is not needed anymore

--- app/db/base.py
+++ app/db/base.py
@@ -1,14 +1,7 @@
 import databases
 import sqlalchemy
 
-from app.core.config import DATABASE_URL
+from ..core.config import DATABASE_URL
 
 metadata = sqlalchemy.MetaData()
 database = databases.Database(DATABASE_URL)
-
-
-# def create_tables():
-#     from sqlalchemy import create_engine
-#     engine = create_engine(DATABASE_URL)
-#     metadata.drop_all(engine)
-#     metadata.create_all(engine)
